66014e7d29e9e8245b9753687e9affd4847e5ac3
EmbedCreativity <embedCreativity@users.noreply.github.com>
2020-08-14 13:45:32 -0400

    Update crypto.c
    GPL notice

--- crypto.c
+++ crypto.c
@@ -1,9 +1,19 @@
 /*
-*    This is a counter wrapper around Gladman's AES engine.  This interface removes much of the detail
-*    involved in the underlying crypto implementation and leaves a simple clean interface.
-*
-*    History:
-*        January 10, 2011:  created
+  Copyright (C) 2020 Embed Creativity LLC
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 #include "crypto.h"
